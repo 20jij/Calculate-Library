041d6bcae7c80855cd5ea6d09f23a8b24c177d0e
APCS2nd <APCS2nd@MHS-Dreyer-10>
2018-10-05 09:54:36 -0700

    Calculate part 5 initial version.

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,12 @@
+//This class contains methods that do the calculations to determines the shape of a quadratic function.
+//@author JasonJi
+//version 10/5/2018
+
+public class Quadratic {
+	public static String quadrDescriber (double a, double b, double c) {
+		discriminant = discriminant(a,b,c);
+		
+	
+		
+	}
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,23 @@
+//This class contains methods that interact with the user and describe a quadratic function.
+//@author JasonJi
+//version 10/5/2018
+
+public class QuadraticClient {
+
+	/**
+	 * 
+	 */
+	public QuadraticClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	
+
+	}
+
+}
