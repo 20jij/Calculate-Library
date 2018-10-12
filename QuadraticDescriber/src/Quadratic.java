95dc7e1e3ddc6e7114a1cf08ac84020f1b3d6dd6
APCS2nd <APCS2nd@MHS-Dreyer-10>
2018-10-12 15:39:29 -0700

    Completed Part 5

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -5,19 +5,21 @@
 public class Quadratic {
 	public static String quadrDescriber (double a, double b, double c) {
 		double discriminant = Calculate.discriminant(a,b,c);
+		String opens;
 		if (a>0) {
-			String opens =  "Up" ;
+			opens =  "Up" ;
 		}
 		else {
-			String opens =  "Down";
+			opens =  "Down";
 		}
 		double symmetryLine = -b/2*a;
 		double vertexX = symmetryLine;
 		double vertexY = a*symmetryLine*symmetryLine+b*symmetryLine+c;
 		double yIntercept = c;
 		String xIntercept = Calculate.quadForm(a, b, c);
-		return "Opens:" + opens \n"Axis of Symmetry:" + symmetryLine \n"Vertex:(" + vertexX + ", " + vertexY + ")" \n 
-	
+		return "Opens:" + opens +"\n" + "Axis of Symmetry:" + symmetryLine + "\n" + "Vertex:(" + vertexX + ", " + vertexY + ")" + "\n" + "x-intercept(s):" + xIntercept + "\n" + "y-intercept:" + yIntercept;
+		
+
 		
 	}
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -8,9 +8,10 @@ public class QuadraticClient {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to the Quadratic Describer");
-		String confirmation = "yes";
+		String confirmation;
+		boolean keepGoing = true;
 		do {
-			System.out.println("Provide values foro coefficients a, b, and c");
+			System.out.println("Provide values for coefficients a, b, and c");
 			System.out.print("a:");
 			Scanner console = new Scanner(System.in);
 			double a = console.nextDouble();
@@ -18,10 +19,16 @@ public class QuadraticClient {
 			double b = console.nextDouble();
 			System.out.print("c:");
 			double c = console.nextDouble();
+			System.out.println(" ");
 			System.out.println("Description of the graph of:");
-			System.out.println("y = " + a + "x^2" + b + "x" + c);
-			System.out.print(Quadratic.quadrDescriber(a,b,c) );
-		}
+			System.out.println("y = " + a + "x^2" + "+" + b + "x" + "+" + c);
+			System.out.println(" ");
+			System.out.println(Quadratic.quadrDescriber(a,b,c) );
+			System.out.println(" ");
+			System.out.println("Do you want to keep going?(Type \"quit\" to end)");
+			confirmation = console.next();
+			keepGoing = !confirmation.equals ("quit");
+		}while (keepGoing == true);
 	
 		
 	
